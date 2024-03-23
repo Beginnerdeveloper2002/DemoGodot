d0121b9bd9054ac17d645199187aa19c8c366711
StanislavBardash <bardashstas@gmail.com>
2024-03-24 00:09:38 +0800

    added something

---
+++ scene/tank.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 0
+#const JUMP_VELOCITY = -400.0
+
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("down"):
+		velocity.y+=SPEED * delta
+	if Input.is_action_pressed("up"):
+		velocity.y-=SPEED * delta
+	else:
+		velocity.y*=0.8
+	move_and_slide()
+	
