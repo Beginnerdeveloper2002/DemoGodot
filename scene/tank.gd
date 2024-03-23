8c7c57bf646dbcac458399ba126a0fbaaea84b3d
StanislavBardash <bardashstas@gmail.com>
2024-03-24 00:19:20 +0800

    solved issue with movement

--- scene/tank.gd
+++ scene/tank.gd
@@ -2,18 +2,12 @@ extends CharacterBody2D
 
 
 @export var SPEED = 0
-#const JUMP_VELOCITY = -400.0
-
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 	if Input.is_action_pressed("down"):
-		velocity.y+=SPEED * delta
+		position.y+=SPEED
 	if Input.is_action_pressed("up"):
-		velocity.y-=SPEED * delta
-	else:
-		velocity.y*=0.8
-	move_and_slide()
+		position.y-=SPEED
+
 	
